6c24ff96956a5964dba435ff7d17d88e1f218ccf
Laken Harville <rlakenharville@gmail.com>
2022-04-12 17:12:52 -0500

    Initial commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,41 +10,38 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            /*
-             * Todo follow all comments!! 
-             */
+           
 
             #region Vehicles
 
-            /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
-             */
+         
 
-            /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
-            */
 
-            // Create a list of Vehicle called vehicles
 
-            /*
-             * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
-             * - new it up as one of each derived class
-             * Set the properties with object initializer syntax
-             */
+            
+            var vehicles = new List<Vehicle>();
+         
+           
+            var camry = new Car() { HasTrunk = true, Make = "Toyota", Year = 2014 };
+            var motorcycle = new Motorcycle() { HasSideCart = true, Make = "Kawasaki", Model = "Ninja", Year = 2005 };
 
-            /*
-             * Add the 4 vehicles to the list
-             * Using a foreach loop iterate over each of the properties
-             */
+            Vehicle sedan = new Car() { HasTrunk = true, Make = "Chevy", Model = "Malibu", Year = 2010 };
+            Vehicle coupe = new Car() { HasTrunk = true, Make = "Honda", Model = "Civic", Year = 2008 };
 
-            // Call each of the drive methods for one car and one motorcycle
+            vehicles.Add(camry);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(coupe);
+
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($"Make: {veh.Make}. Model: {veh.Model}. Year: {veh.Year}.");
+                veh.DriveAbstract();
+                Console.WriteLine();
+            }
+            
+
+
 
             #endregion            
             Console.ReadLine();
